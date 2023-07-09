830b421686d59a18354d82a91f4b270991527fe7
NiharGharat <nihar.gharat@gmail.com>
2023-07-09 11:47:59 -0500

    Almost done with intro, wip on colouring etc. Didnt add images

--- portfolio-nextjs/pages/intro.js
+++ portfolio-nextjs/pages/intro.js
@@ -10,41 +10,30 @@ export default function Intro() {
             <link rel="icon" href="/favicon.ico" />
         </Head>
         <main className="bg-gray-100 m-3">
-            <section className="grid grid-cols-3 border-solid border-2 border-black">
-                <div className="p-3">
-                    <img src="/images/mock_icon.jpeg"  alt="Profile icon" className="h-12"/>
+            <section className="grid grid-cols-2 gap-4 border-solid border-2 border-black content-center shadow-lg">
+                <div className="">
+                    <img src="/images/mock_icon.jpeg" alt="Profile icon" className="h-16 m-2 pl-2"/>
                     {/* TODO - Icon here */}
                 </div>
-                <div className="p-3 text-center text-4xl font-bold">
-                    {/* TODO - This can be clickable in the future which leads to Musin */}
-                    <div>Nihar Gharat</div>
-                    <div className="text-xs font-normal">software developer</div>
-
+                <div className="grid grid-cols-3 text-center text-lg content-center">
+                    <span className="m-3 p-3 bg-indigo-300"><Link href={"./experience"}>Experience</Link></span>
+                    <span className="m-3 p-3 bg-indigo-400"><Link href={"./project"}>Projects</Link></span>
+                    <span className="m-3 p-3 bg-indigo-500"><Link href={"./contact"}>Contact</Link></span>
                 </div>
-                <div className="inline-block text-center align-middle">
-                    <span>E</span>
-                    <span>P</span>
-                    <span>C</span>
-                </div>
-                {/* <div className="p-3 text-center grid grid-cols-3 content-center">
-                    <Link href={"./experience"} className="rounded-full h-7 w-7 bg-blue-400"><div className="">E</div></Link>
-                    <Link href={"./projects"} className="rounded-full h-7 w-7 bg-blue-400"><div className="">P</div></Link>
-                    <Link href={"./contact"} className="rounded-full h-7 w-7 bg-blue-400"><div className="">C</div></Link>
-                </div> */}
             </section>
             <section className="p-3 grid grid-cols-2 gap-4">
-                <div className="pt-3 grid grid-rows-2">
-                    <div className="px-3 text-xl font-bold">
-                        {/* #/home/nihar/aboutMe */}
-                    </div>
-                    <div className="p-3 text-2xl text-gray-700 border-solid border-2 border-l-gray-800 border-b-gray-800 border-r-gray-100 border-t-gray-100">
-                        <div>hi, I am <span className="font-bold text-gray-800 text-4xl">Nihar</span></div>
-                        <div className="text-xl mt-2">I am a backend software developer currently based in US. I am passionate about working on interesting ideas/projects.</div>
-                        <div className="text-xl mt-2"><Link href="#">Check out my work</Link></div>
+                <div className="pt-3">
+                    <div className="p-3 text-xl space-y-2 text-gray-700">
+                        <div className="text-6xl font-bold">About Me</div>
+                        <div className="pt-4">Hi, I am <span className="font-bold text-3xl">Nihar Gharat</span></div>
+                        <div className="text-lg pt-2">I am a backend software developer currently based in US. I am passionate about working on interesting ideas/projects.</div>
+                        <div className="text-lg pt-2">I thoroughly enjoy engaging with complex problems, exploring potential solutions, and crafting well-thought-out resolutions. Adopting a designer's mindset, I approach every challenge with creativity and attention to detail. Through fortunate interactions with individuals in diverse fields such as Designing and Architecture, I found a genuine connection. I bring this unique perspective and problem-solving approach to the forefront.</div>
+                        <div className="text-lg pt-2">I loved working on my projects, well that was a part of the reason why I picked them in the first place. Right from removing the mundanity to be assured of my music playlists being backed via the youtubeMusic Playlist Scrapper, to diving deep in the neural network and data mining with TailorSweat, I enjoyed it all.</div>
+                        <div className="mt-4 inline-block bg-indigo-400 rounded-lg px-4 py-2 tracking-tighter"><Link href="#">Check out my work</Link></div>
                     </div>
                 </div>
                 <div>
-                    <img src={"/images/mock_intro.jpeg"} alt="Profile image" className="h-70 shadow-xl"/>
+                    <img src={"/images/mock_intro_2.jpeg"} alt="Profile image" className="mt-6 h-70 bg-gray-100 shadow-lg"/>
                 </div>
             </section>
         </main>

52df204865573754c7e49242d662550bc9b1b54a
NiharGharat <nihar.gharat@gmail.com>
2023-07-13 00:46:50 -0500

    Done with responsiveness of experience

--- portfolio-nextjs/pages/experience.js
+++ portfolio-nextjs/pages/experience.js
@@ -9,14 +9,15 @@ export default function Experience(props) {
             1. Create the cards
             2. Font reponsiveness
             3. Card responsiveness
-            4. 
+            4. Extract components
+            5. Write correct info data
             */}
             {console.log(props.allExpData)}
             <Head>
                 <title>Work Experience</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main className="bg-gray-100 m-2 p-2 mx-auto max-w-lg sm:max-w-xl md:max-w-4xl lg:max-w-5xl">
+            <main className="bg-gray-100 m-2 p-2 mx-auto max-w-lg sm:max-w-xl md:max-w-4xl lg:max-w-6xl">
                 <section className="grid grid-cols-1 sm:grid-cols-3 gap-2 md:gap-4 sm:border-solid border-2 border-black content-center shadow-xl">
                     <div className="hidden sm:block sm:col-span-1">
                         <img src="/images/mock_icon.jpeg" alt="Profile icon" className="h-16 sm:h-20 md:h-24 lg:h-28 m-2 pl-0"/>
@@ -29,9 +30,14 @@ export default function Experience(props) {
                     </div>
                 </section>
                 {/* Now the main body */}
-                <section className="p-3 grid grid-cols-1 md:grid-cols-3">
+                <section className="p-3 grid grid-cols-1 md:grid-cols-3 md:gap-2 lg:gap-4">
                     <div className="pt-3 text-xl text-gray-700 col-span-1">
-                        <div className="p-3 text-4xl md:text-5xl px-3 font-bold text-center">Work Experience</div>
+                        <div className="p-3 text-4xl px-3 font-bold text-center">Work Experience</div>
+                        {/* TODO */}
+                        <div className='p-3 text-sm border-2 border-gray-800 text-justify'>
+                            <div className='mt-2'>I loved to work here. This and that. I like to work in product engineering mainly focused in development</div>
+                            <div className='mt-2'>I loved to work here. This and that. I like to work in product engineering mainly focused in development</div>
+                        </div>
                         {/* The short desc of work exp if necessary */}
                     </div>
                     <div className="sm:px-2 md:col-span-2">
